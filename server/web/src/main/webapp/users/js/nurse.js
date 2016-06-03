08088b0600b50e868a8f8d8e283d02ed31cde72e
HZCJ <luffy.ja@gmail.com>
2016-06-03 21:04:12 +0800

    add nurse

--- server/web/src/main/webapp/users/js/nurse.js
+++ server/web/src/main/webapp/users/js/nurse.js
@@ -4,6 +4,31 @@
 
 $(
     function () {
+
+        $('#nurseForm').validate({
+            ignoreTitle: true,
+            highlight: function (element) {
+                $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+            },
+            success: function (element) {
+                $(element).closest('.form-group').removeClass('has-error');
+                $(element).remove();
+            },
+            onfocusout: function (element) {
+                $(element).valid();
+            },
+            rules: {
+                username: {
+                    required: true,
+                    minlength: 3
+                },
+                password: {
+                    required: true,
+                    minlength: 5
+                }
+            }
+        });
+
         jQuery('#nurseTable').dataTable({
             language: {
                 url: dataTable_localisation_url
